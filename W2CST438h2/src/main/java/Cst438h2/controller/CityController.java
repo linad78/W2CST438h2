91718afeb0c3aae0a3d975989ecaca21ca31c147
George Blombach <george.blombach@pacificomputing.com>
2020-07-07 19:17:48 -0700

    You should not be using a ResponseEntity here. The ResponseEntity is for the Rest Controller. This needs to be setup and tested similar to how we did HW1. When navigating to localhost:8080/cities/<city> you should be able to see the returned data. So, I could not verify the other downstream modules are working through this controller.
    So, add two templates for the two conditions, data and no data returned,
    and then change this method to something that will return data from the
    query. See in-code comments for example.

--- W2CST438h2/src/main/java/Cst438h2/controller/CityController.java
+++ W2CST438h2/src/main/java/Cst438h2/controller/CityController.java
@@ -19,6 +19,33 @@ public class CityController {
 	private CityService cityService;
 	
 	@GetMapping("/cities/{city}")
+	
+	/**Comments from George
+	 * You should not be using a ResponseEntity here. The ResponseEntity is for the Rest
+	 * Controller. This needs to be setup and tested similar to how we did HW1. When navigating
+	 * to localhost:8080/cities/<city> you should be able to see the returned data. So, I could
+	 *  not verify the other downstream modules are working through this controller.
+	 * 
+	 *  So, add two templates for the two conditions, data and no data returned, and then
+	 *  change this method to something that will return data from the query. Here
+	 *  is how I wrote mine:
+	 *  
+		public String getCityInfo(@PathVariable("city") String cityName, Model model) {
+		
+		CityInfo cityInfo = cityService.getCityInfo(cityName);
+		
+		if (cityInfo != null) {
+			model.addAttribute("cityInfo", cityInfo);
+			return "city_info";
+		}
+		else {
+			model.addAttribute("time", new java.util.Date().toString());
+			return "error";
+		}
+     } 
+	 * 
+	 */
+	
 	public ResponseEntity<Cst438h2.domain.City> getWeather(@PathVariable("city") String cityName, Model model, CityInfo Null) {
 
 		// TODO Your code goes here
